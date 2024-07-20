34b38d795c6f886e4aab41ecf925dd64a469527c
Mark Sinclair Jr <mark.edward.x+gitlab@gmail.com>
2024-07-19 21:37:54 -0500

    basic parsing of world
    Co-authored-by: Octavia <shawoj93@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,60 +5,64 @@ pub struct Conway {
     // what goes here?
     pub cells: Vec<bool>,
     pub rows: usize,
-    pub cols: usize
+    pub cols: usize,
 }
 
 impl Conway {
     pub fn get(&self, row: usize, col: usize) -> bool {
-        todo!()
+        // row * cols + col
+        let index = row  * self.cols + col;
+        self.cells[index]
     }
 }
 
 pub fn parse(input: &str) -> Conway {
     let lines = input.split('\n');
-    let lines: Vec<_>  = lines.collect();
+    let lines: Vec<_> = lines.collect();
 
     let [first, second, ..] = lines.as_slice() else {
         panic!("Help");
     };
 
-    let [rows, col, ..] = first.split('x').collect::<Vec<_>>().as_slice() else {
+    let dimension_fields = first.split('x').collect::<Vec<_>>();
+    let [rows, cols, ..] = dimension_fields.as_slice() else {
         panic!("Help")
     };
 
-    
-    todo!()
+    let rows: usize = rows.parse().unwrap();
+    let cols: usize = cols.parse().unwrap();
+
+    // Turn second string into a vec<bool>
+    let cells: Vec<bool> = second
+        .chars()
+        .map(|char| -> bool { char.to_digit(2).unwrap() != 0 })
+        .collect();
+
+    //Return a conway
+    Conway {
+        cells: cells,
+        rows: rows,
+        cols: cols,
+    }
 }
-
-// pub Result<T, E> {
-//     Ok(T),
-//     Err(E)
-// }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: & 'static str =  "3x3\n010101010";
+    const INPUT: &'static str = "2x3\n010101";
 
     #[test]
     fn parse_simple() {
         let conway = parse(INPUT);
 
-        assert_eq!(conway.get(0,0), false);
-        assert_eq!(conway.get(0,1), true);
-        assert_eq!(conway.get(0,2), false);
-        assert_eq!(conway.get(1,0), true);
-        assert_eq!(conway.get(1,1), false);
-        assert_eq!(conway.get(1,2), true);
-        assert_eq!(conway.get(2,0), false);
-        assert_eq!(conway.get(2,1), true);
-        assert_eq!(conway.get(2,2), false);
-        // assert_eq!(result, 4);
+        assert_eq!(conway.get(0, 0), false);
+        assert_eq!(conway.get(0, 1), true);
+        assert_eq!(conway.get(0, 2), false);
+        assert_eq!(conway.get(1, 0), true);
+        assert_eq!(conway.get(1, 1), false);
+        assert_eq!(conway.get(1, 2), true);
     }
-
-
 }
 
 // text
@@ -72,6 +76,14 @@ mod tests {
 // Any live cell with more than three live neighbours dies, as if by overpopulation.
 // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-
-// 3x3 
+// rows x columns
+// <cells as bits on or off in one line>
+// example below
+//
+// 3x3
 // 010101010
+// (0,0),(0,1),(0,2),(1,0),(1,1),(1,2)
+// 0,1,2,3,4,5
+// row|row|row
+// 3 cells per row
+//  rc = p
