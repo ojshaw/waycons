bfbce05258ab4e8c88fec241d0c72b37e69ec549
Mark Sinclair Jr <mark.edward.x+gitlab@gmail.com>
2024-08-10 17:03:02 -0500

    feat: count up living neighbors

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-use std::collections::HashSet;
-
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Conway {
     // what goes here?
     pub cells: Vec<bool>,
@@ -11,9 +9,126 @@ pub struct Conway {
 impl Conway {
     pub fn get(&self, row: usize, col: usize) -> bool {
         // row * cols + col
-        let index = row  * self.cols + col;
+        let index = row * self.cols + col;
         self.cells[index]
     }
+
+    pub fn update(self) -> Conway {
+        todo!()
+    }
+
+    fn count_live_neighbors(&self, row: usize, col: usize) -> usize {
+        assert!(self.rows > 0);
+        assert!(self.cols > 0);
+
+        // generating coords
+        let neighbor_coords = match self.detect_kind(row, col) {
+            CellKind::TopLeft => vec![(0, 1), (1, 1), (1, 0)],
+            CellKind::Top => vec![
+                (0, col - 1),
+                (0, col + 1),
+                (1, col - 1),
+                (1, col),
+                (1, col + 1),
+            ],
+            CellKind::TopRight => vec![
+                (0, self.col_max() - 1),
+                (1, self.col_max() - 1),
+                (1, self.col_max()),
+            ],
+            CellKind::Right => vec![
+                (row - 1, col),
+                (row + 1, col),
+                (row - 1, col - 1),
+                (row, col - 1),
+                (row + 1, col - 1),
+            ],
+            CellKind::BottomRight => vec![(row - 1, col), (row - 1, col - 1), (row, col - 1)],
+            CellKind::Bottom => vec![
+                (row, col - 1),
+                (row, col + 1),
+                (row - 1, col - 1),
+                (row - 1, col),
+                (row - 1, col + 1),
+            ],
+            CellKind::BottomLeft => vec![(row - 1, col), (row - 1, col + 1), (row, col + 1)],
+            CellKind::Left => vec![
+                (row - 1, 0),
+                (row + 1, 0),
+                (row - 1, 1),
+                (row, 1),
+                (row + 1, 1),
+            ],
+            CellKind::Middle => vec![
+                (row - 1, col - 1),
+                (row - 1, col),
+                (row - 1, col + 1),
+                (row, col + 1),
+                (row + 1, col + 1),
+                (row + 1, col),
+                (row + 1, col - 1),
+                (row, col - 1),
+            ],
+        };
+
+        // loop through neighbors and count the living
+        // let mut life_count = 0;
+        // for (row, col) in neighbor_coords {
+        //     if self.get(row,col) {
+        //         life_count += 1;
+        //     }
+        // }
+        // life_count
+
+        neighbor_coords
+            .into_iter()
+            .fold(0, |count, (row, col)| count + (self.get(row, col) as usize))
+    }
+
+    fn has_no_edges(&self, row: usize, col: usize) -> bool {
+        row != 0 && row < self.rows && col != 0 && col < self.cols
+    }
+
+    fn has_one_edge(&self, row: usize, col: usize) -> bool {
+        (row == 0 || row == self.row_max()) != (col == 0 || col == self.col_max())
+    }
+
+    fn has_two_edges(&self, row: usize, col: usize) -> bool {
+        (row == 0 || row == self.row_max()) && (col == 0 || col == self.col_max())
+    }
+
+    fn detect_kind(&self, row: usize, col: usize) -> CellKind {
+        match (row, col) {
+            (0, 0) => CellKind::TopLeft,
+            (0, c) if c == self.col_max() => CellKind::TopRight,
+            (r, 0) if r == self.row_max() => CellKind::BottomLeft,
+            (r, c) if r == self.row_max() && c == self.col_max() => CellKind::BottomRight,
+            (0, _) => CellKind::Top,
+            (r, _) if r == self.row_max() => CellKind::Bottom,
+            (_, 0) => CellKind::Left,
+            (_, c) if c == self.col_max() => CellKind::Right,
+            _ => CellKind::Middle,
+        }
+    }
+
+    const fn col_max(&self) -> usize {
+        self.cols - 1
+    }
+    const fn row_max(&self) -> usize {
+        self.rows - 1
+    }
+}
+
+enum CellKind {
+    TopLeft, //
+    Top,
+    TopRight,
+    Right,
+    BottomRight,
+    Bottom,
+    BottomLeft,
+    Left,
+    Middle,
 }
 
 pub fn parse(input: &str) -> Conway {
@@ -31,6 +146,8 @@ pub fn parse(input: &str) -> Conway {
 
     let rows: usize = rows.parse().unwrap();
     let cols: usize = cols.parse().unwrap();
+    assert!(rows > 0);
+    assert!(cols > 0);
 
     // Turn second string into a vec<bool>
     let cells: Vec<bool> = second
@@ -51,6 +168,14 @@ mod tests {
     use super::*;
 
     const INPUT: &'static str = "2x3\n010101";
+    // oxo
+    // xox
+    // becomes
+    // oxo
+    // oxo
+    // becomes
+    // ooo
+    // ooo
 
     #[test]
     fn parse_simple() {
@@ -63,12 +188,23 @@ mod tests {
         assert_eq!(conway.get(1, 1), false);
         assert_eq!(conway.get(1, 2), true);
     }
+
+    #[test]
+    fn step_simple() {
+        let conway = parse(INPUT);
+
+        let c = conway.update();
+        assert_eq!(c, parse("2x3\n010010"));
+
+        let c2 = c.update();
+        assert_eq!(c2, parse("2x3\n000000"));
+    }
 }
 
 // text
 // single
 // standard rules
-// wrap around the edges
+// borders: closed, then wrapping, then infinite???
 
 // RULES
 // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
